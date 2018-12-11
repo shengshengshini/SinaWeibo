b4b7559e668ce80dc69e68b1fd0a57752e0386e5
shengshengshini <2321837783@qq.com>
2018-12-12 00:25:23 +0800

    一个简单的Chrome微博插件，去除广告，去掉不必要的推荐，增强阅读体验。

---
+++ app.js
@@ -0,0 +1,19 @@
+$(function() 
+{
+
+    $(".input textarea").val("没有啥事需要说的额");
+  /*
+  console.log("loaded");
+
+  console.log($("#v6_pl_rightmod_attfeed"));
+  $("#v6_pl_rightmod_attfeed").remove();
+
+  setTimeout( ()=> 
+  {
+  	$("#v6_pl_rightmod_attfeed").remove();
+  	console.log("找不熬~");
+  } , 2000 );
+
+  */
+  
+});
